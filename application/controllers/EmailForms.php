695d3a34c4facdf024d9b2b5a3f50fa2f7fe410b
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2022-08-17 17:36:03 -0700

    Added mail function.

--- application/controllers/EmailForms.php
+++ application/controllers/EmailForms.php
@@ -1,16 +1,16 @@
 <?php
 
-class Forms extends CI_Controller{
+class EmailForms extends CI_Controller{
 
-	public function create()
+	public function send()
 	{
 		$data['title'] = 'Send Email';
 
 
-		$this->form_validation->set_rules('name', 'Name', 'required');
-		$this->form_validation->set_rules('email', 'Email', 'required');
-		$this->form_validation->set_rules('subject', 'Subject', 'required');
-		$this->form_validation->set_rules('message', 'Message', 'required');
+		$name = $this->form_validation->set_rules('name', 'Name', 'required');
+		$email = $this->form_validation->set_rules('email', 'Email', 'required');
+		$subject = $this->form_validation->set_rules('subject', 'Subject', 'required');
+		$message = $this->form_validation->set_rules('message', 'Message', 'required');
 
 
 		$this->form_validation->set_error_delimiters(
@@ -28,9 +28,16 @@ class Forms extends CI_Controller{
 		}
 		else
 		{
+			$to = "info@leanquatro.com, info@nexussoftwaresolutions.com";
+			$subject = "$subject";
+			$message = "$message  <br>$name<br>$email";
+			$headers = "MIME-Version: 1.0" . "\r\n";
+			$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+			$headers .= 'From: <info@leanquatro.com>' . "\r\n";
 
-			//$this->FormModel->create_sup();
-			$this->FormModel->sendMail();
+			mail($to,$subject,$message,$headers);
+
+
 
 			//session message
 			$this->session->set_flashdata('sent', 'Email has been sent.');
@@ -39,87 +46,6 @@ class Forms extends CI_Controller{
 		}
 
 	}
-
-
-	public function request($id = NULL)
-	{
-		$data['title'] = 'Request';
-		$data['sup'] = $this->FormModel->get_single_sup($id);
-		$data['details'] = $this->FormModel->get_single_sup_details($id);
-
-		$this->form_validation->set_rules('id', 'ID', 'required');
-		$this->form_validation->set_rules('contador', 'Seleccion de partes', 'required');
-		$this->form_validation->set_rules('checked', 'Insertos seleccionados', 'required|greater_than[0]');
-		$this->form_validation->set_rules('location', 'Planta', 'required');
-
-
-		$this->form_validation->set_error_delimiters(
-			'<div class="alert alert-danger alert-dismissible fade show" role="alert"><strong class="uppercase"><bdi>Error</bdi></strong> &nbsp;',
-			'<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
-		);
-
-		if($this->form_validation->run() === FALSE)
-		{
-			$this->load->view('templates/header');
-			$this->load->view('forms/request', $data);
-			$this->load->view('templates/footer');
-		}
-		else
-		{
-
-			$this->FormModel->request();
-
-			//session message
-			$this->session->set_flashdata('pedido', 'Se ha pedido el molde.');
-
-
-
-			//email test
-
-			$this->load->library('email');
-
-			$subject = 'This is a test';
-			$message = '<p>This message has been sent for testing purposes.</p>';
-
-			$result = $this->email
-				->from('jgomez@martechmedical.com')
-				->reply_to('noreply@martechmedical.com')    // Optional, an account where a human being reads.
-				->to('jgomez@martechmedical.com')
-				->subject($subject)
-				->message($message)
-				->send();
-
-			var_dump($result);
-			echo '<br />';
-			echo $this->email->print_debugger();
-
-			exit;
-
-
-			//email test end
-
-			//redirect(base_url() . 'forms/request');
-			redirect(base_url());
-		}
-
-	}
-
-
-
-	public function get_insert_details()
-	{
-		// POST data
-		$postData = $this->input->post();
-
-		// load model
-		$this->load->model('FormModel');
-
-		// get data
-		$data = $this->FormModel->get_insert_details($postData);
-		echo json_encode($data);
-	}
-
-
 
 
 
