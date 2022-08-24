4725c8e484b28658732fdb8cfd53c088fadaec86
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2022-08-24 15:11:19 -0700

    Changed config/routes to include contact.

--- application/config/routes.php
+++ application/config/routes.php
@@ -9,8 +9,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 //pages
+$route['contact'] = 'contact/index';
 $route['(:any)'] = 'pages/view/$1';
 $route['default_controller'] = 'pages/view';
-$route['contact'] = 'contact/index';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
